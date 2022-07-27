5541d7d4f11505fb9e5ec0bf6a80c3155556a9f8
Scott Chau <scottshing@yahoo.com.hk>
2022-07-27 13:28:00 -0400

    calculator mode setup

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(num1, num2, callback) {
+  return callback(num1, num2);
+}
+
+console.log(operate(3, 3, divide));
